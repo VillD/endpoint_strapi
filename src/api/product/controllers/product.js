5943d8cc4636f82cc098ab17ea348f931edffc5f
VillD <rodriguezvillegasdl@gmail.com>
2022-10-10 14:24:12 -0500

    strapi-data

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,32 +1,32 @@
-const axios = require('axios');
-'use strict';
+const axios = require("axios");
+("use strict");
 
 /**
  * product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product',({ strapi })=>({
-    dataProduct: async(ctx, next)=>{
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  dataProduct: async (ctx, next) => {
+    // const {id} = ctx.request.params;
+    // const user = ctx.state.user;
+    // const product = await strapi.entityService.findOne("api::product.product", id);
+    // console.log(ctx.state.user)
+    // return id
 
-        const {id} = ctx.request.params;
-        const user = ctx.state.user;
-        const product = await strapi.entityService.findOne("api::product.product", id);
-        console.log(ctx.state.user)
-        return id
-        
-        // try{
-        //     const response = await axios.get(ctx.request.params);
-        //     console.log(response)
-        //     return response;
-        // }
-        // catch(error){
-        //     console.log(console.error)
-        // }
-        
-        /*
+    try {
+      const url = "https://catfact.ninja/fact";
+      const response = await axios.get(url);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+
+    /*
         const response = await axios.get('/user?ID=12345');
         console.log(response);*/
-    }
+  },
 }));
